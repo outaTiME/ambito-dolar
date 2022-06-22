9fb5fa51225c183825970bd72d96c3991b4857cd
outaTiME <afalduto@gmail.com>
2022-06-22 20:34:09 -0300

    style: comment cleanup

--- packages/api/src/subscribers/social-notify.js
+++ packages/api/src/subscribers/social-notify.js
@@ -30,7 +30,6 @@ const generateScreenshot = async (type, title) => {
   await page.goto(screenshot_url, { waitUntil: 'networkidle0' });
   // https://pptr.dev/#?product=Puppeteer&version=v5.2.1&show=api-pageselector
   // FIXME: throw exception when invalid page contents using page.$$('svg')
-  // FIXME: update image type to png ??? or leave default ???
   // const image_type = 'jpeg';
   const file = await page.screenshot({
     // type: image_type,
