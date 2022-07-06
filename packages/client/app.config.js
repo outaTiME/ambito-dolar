1053626ab75592e4fee351962aef18b2ac15f038
outaTiME <afalduto@gmail.com>
2022-07-06 10:49:00 -0300

    build: bump version and build number

--- packages/client/app.config.js
+++ packages/client/app.config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
-const version = '6.0.2';
-const buildNumber = 72;
+const version = '6.0.3';
+const buildNumber = 73;
 
 const LIGHT_SPLASH = {
   image: './assets/splash-light.png',
