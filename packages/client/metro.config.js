907728f13337e2b633dc75e9ff425fb8eb96f174
outaTiME <afalduto@gmail.com>
2025-09-20 20:42:31 -0300

    chore: improve metro bundler config for monorepos

--- packages/client/metro.config.js
+++ packages/client/metro.config.js
@@ -1,22 +1,6 @@
 /* eslint-env node */
-// Learn more https://docs.expo.dev/guides/monorepos
-// const { getDefaultConfig } = require('expo/metro-config');
+// const { getDefaultConfig } = require("expo/metro-config");
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const path = require('path');
-
-// Find the project and workspace directories
-const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, '../..');
-
-const config = getSentryExpoConfig(projectRoot);
-
-// 1. Watch all files within the monorepo
-config.watchFolders = [monorepoRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
-];
-
+// const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 module.exports = config;
