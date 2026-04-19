a4da646638b6c6a5a67163d363d879079544bff1
outaTiME <afalduto@gmail.com>
2026-04-19 19:06:00 -0300

    chore: unify eslint configuration across workspaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
 module.exports = {
+  root: true,
   extends: 'universe/node',
+  ignorePatterns: [
+    '.sst/**',
+    'packages/website/.cache/**',
+    'packages/website/public/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/.expo/**',
+  ],
+  overrides: [
+    {
+      files: ['packages/client/**/*.{js,jsx,ts,tsx}'],
+      extends: 'universe/native',
+    },
+    {
+      files: ['packages/website/**/*.{js,jsx,ts,tsx}'],
+      extends: 'universe/web',
+    },
+  ],
 };
