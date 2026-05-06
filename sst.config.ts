9be8123b8cb3d3f7be574eca0f68727aa2240f50
outaTiME <afalduto@gmail.com>
2026-05-06 17:26:36 -0300

    feat: enable SST state file compression

--- sst.config.ts
+++ sst.config.ts
@@ -7,6 +7,9 @@ export default $config({
       home: 'aws',
       region: 'us-east-1',
       removal: input?.stage === 'prod' ? 'retain' : 'remove',
+      state: {
+        compress: true,
+      },
     };
   },
   async run() {
