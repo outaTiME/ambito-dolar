99dc28ee186936ace777fbc3eb12068ce674de83
outaTiME <afalduto@gmail.com>
2026-09-15 20:14:53 -0300

    fix: align the donation note with the rest of the footnotes

--- packages/client/config/I18n.ts
+++ packages/client/config/I18n.ts
@@ -68,7 +68,7 @@ const i18n = new I18n({
     donate: 'Donar',
     donate_choose_title: 'Elegí tu aporte',
     donate_choose_note:
-      'Son de cobro único y podés hacerlo cuantas veces quieras.',
+      'Los aportes son de cobro único y podés hacerlos cuantas veces quieras.',
     donate_modal_note:
       'Elegí tu aporte y acompañá el mantenimiento y desarrollo continuo de esta aplicación.',
     donate_product_small_contribution: 'Pequeño',
