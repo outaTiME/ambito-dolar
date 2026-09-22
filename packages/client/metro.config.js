af5af4dbb2301aafa3614f59b7000908369389d6
outaTiME <afalduto@gmail.com>
2026-09-21 21:13:44 -0300

    fix: restore the metro victory alias

--- packages/client/metro.config.js
+++ packages/client/metro.config.js
@@ -6,8 +6,7 @@ const config = getSentryExpoConfig(__dirname);
 config.resolver = config.resolver || {};
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
-  // resolved and not joined, the package is hoisted and does not sit here
-  'victory-native': path.dirname(require.resolve('victory/package.json')),
+  'victory-native': path.resolve(__dirname, 'node_modules/victory'),
 };
 
 module.exports = config;
